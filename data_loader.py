132ad1d5d0d417aad52e99b3e912cead0005863a
Carson Schubert <carson.schubert14@gmail.com>
2019-03-15 14:59:31 -0500

    dataset created

--- data_loader.py
+++ data_loader.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
 def get_labels(run_no):
-    file = open('Run'+str(run_no)+'/picOutput/LABELS.txt')
+    file = open('data/run'+str(run_no)+'/picOutput/LABELS.txt')
     lines = file.read().splitlines()
+    # print(lines[0])
     return lines
-    #print(lines[0])
 def parse_lbl(lbl):
     meta_data = lbl.split('LogTemp: RTMAT ')[1].split(' ')
     img_mat = meta_data[2]
@@ -14,20 +14,38 @@ def parse_lbl(lbl):
 
 def load_img(lbl,extension = '/picOutput/im_1230'):
     return cv2.imread(extension+lbl[0]+'_'+lbl[1]+'.png')
+
+def build_dict(num_labels):
+    label_dict = {}
+    for i in range(num_labels):
+        label_dict[i] = []
+    return label_dict
+    
 def load_all(run_no,mode = 0):
+    label_dict = build_dict(6)
     labels = get_labels(run_no)
     imgs = []
     lbls = []
     for lbl in labels:
         one_hot = [0,0,0,0,0,0]
         meta = parse_lbl(lbl)
-        img = load_img((meta[1],meta[0]),'Run'+str(run_no)+'/picOutput/im_1230')
+        # meta: index 0: Image Index, Index 1: Image Page Index, Index 2: Label
+        img = load_img((meta[1],meta[0]),'data/run'+str(run_no)+'/picOutput/im_1230')
         if img is None:
             continue
         imgs.append(np.array(img))
         one_hot[int(meta[2])] = 1
         lbls.append(one_hot)
+
+        path_str = 'data/run'+str(run_no)+'/picOutput/im_1230'+str(meta[1])+'_'+str(meta[0])+'.png'
+        label_dict[int(meta[2])].append(path_str)
     if mode:
         np.save('numpy_saved',[imgs,lbls])
         
-    return imgs,lbls
+    return label_dict
+    
+# imgs, labels, label_dict = load_all(9)
+#print(imgs)
+#print(labels)
+# label_dict = load_all(9)
+# print (label_dict)
