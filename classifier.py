9d5dc90802a5f91ca3887de7892f234338d9a39e
Carson Schubert <carson.schubert14@gmail.com>
2019-03-16 03:26:25 -0500

    fix structure

--- classifier.py
+++ classifier.py
@@ -8,9 +8,10 @@ class Classify(object):
         self._path = artifact_path
         self._learn = load_learner(self._path, fname=fname)
 
-    def predict_numpy(self, image):
+    def predict(self, image):
         pil = PILImage.fromarray(image)
         img = pil2tensor(pil, np.float32).div_(255)
         predImg = Image(img)
-        return self._learn.predict(predImg)
+        category, _, confidence = self._learn.predict(predImg)
+        return int(category.obj), confidence
         
